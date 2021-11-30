39bf1893f8c098809c40fd3469ac3d46f161ba1f
jll19gGH <81544896+jll19gGH@users.noreply.github.com>
2021-11-30 12:29:06 -0500

    added comments and added error check for no selection
    if user does not choose anything, the selection page just keeps being reloaded until they do or go somewhere else

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def recipe_home():
     return render_template('home.html')
     con.close()
 
-@app.route('/see_saved') #Save recipes
+@app.route('/see_saved') #See Saved recipes
 def see_saved():
-    with sql.connect("recipeData.db") as con:  # clears previous data from the table
+    with sql.connect("recipeData.db") as con:
         cur = con.cursor()
         con.row_factory = sql.Row
         cur = con.cursor()
@@ -28,26 +28,38 @@ def see_saved():
         revRows = cur.fetchall();
         return render_template("savedrecipes.html", revRows=revRows)
 
-@app.route('/save_recipe/<id>') #Save recipes
-def save_recipe(id):
+@app.route('/clear_recipes') #clear saved recipes
+def clear_recipes():
     with sql.connect("recipeData.db") as con:  # clears previous data from the table
+        cur = con.cursor()
+        cur.execute("DELETE FROM saved_recipes")
+
+        con.row_factory = sql.Row   #re-displays table
+        cur = con.cursor()
+        cur.execute(f"select * from saved_recipes")
+        revRows = cur.fetchall();
+        return render_template("savedrecipes.html", revRows=revRows)
+
+@app.route('/save_recipe/<id>') #Save recipes (id is the name of the recipe the user wants to save)
+def save_recipe(id):
+    with sql.connect("recipeData.db") as con:  # finds the row in recipes table where the name is equal to the name the user wants to save and inserts it into saved_recipes table
         cur = con.cursor()
         cur.execute("INSERT INTO saved_recipes SELECT * FROM recipes WHERE RecipeName=?", (id,))
 
-        conn = sql.connect("recipeData.db")
+        conn = sql.connect("recipeData.db") #re-displays the list of recipes so the user can continue browsing
         conn.row_factory = sql.Row
         cur = conn.cursor()
         cur.execute(f"select * from recipes")
         revRows = cur.fetchall();
         return render_template("recipes.html", revRows=revRows)
 
-@app.route('/delete_recipe/<id>') #Save recipes
+@app.route('/delete_recipe/<id>') #delete a recipe from the saved list (id is the recipe name)
 def delete_recipe(id):
-    with sql.connect("recipeData.db") as con:  # clears previous data from the table
+    with sql.connect("recipeData.db") as con:  # deletes the row where the name is equal to the name the user wants removed
         cur = con.cursor()
         cur.execute("DELETE FROM saved_recipes WHERE RecipeName=?", (id,))
 
-        con.row_factory = sql.Row
+        con.row_factory = sql.Row   #re-displays the remaining saved recipes
         cur = con.cursor()
         cur.execute(f"select * from saved_recipes")
         revRows = cur.fetchall();
@@ -63,20 +75,20 @@ def get_recipes():
 
         try:
             tag = request.form['recipe_tag']    #gets the tags the user chose
-            tag = tag.replace(" ", "+")
-            inc_ingr=request.form['inc_ingr']
-            inc_ingr = inc_ingr.replace(" ", "+")
-            exc_ingr=request.form['exc_ingr']
-            exc_ingr = exc_ingr.replace(" ", "+")
+            tag = tag.replace(" ", "+") #replace spaces with + to fit URL format
+            inc_ingr=request.form['inc_ingr']   #gets included ingredients the user chose
+            inc_ingr = inc_ingr.replace(" ", "+")   #replace spaces with + to fit URL format
+            exc_ingr=request.form['exc_ingr']   #gets excluded ingredients the user chose
+            exc_ingr = exc_ingr.replace(" ", "+")   #replace spaces with + to fit URL format
 
         except:
-            tag = "NULL" #defaults to Italian if nothing was entered
+            tag = "NULL" #defaults to NULL if nothing was entered
             inc_ingr = request.form['inc_ingr']
             inc_ingr = inc_ingr.replace(" ", "+")
             exc_ingr = request.form['exc_ingr']
             exc_ingr = exc_ingr.replace(" ", "+")
 
-        try:
+        try:    #constructs the URL based on which option(s) were chosen
             if(tag!="NULL" and inc_ingr!="NULL" and exc_ingr=="NULL"):
                 url = 'https://www.allrecipes.com/search/results/?IngIncl='+inc_ingr+'&search='+tag  #constructing the search URL, adding the user's desired criteria
             elif (tag == "NULL" and inc_ingr != "NULL" and exc_ingr == "NULL"):
@@ -89,19 +101,22 @@ def get_recipes():
                 url = 'https://www.allrecipes.com/search/results/?IngExcl='+exc_ingr+'&IngIncl='+inc_ingr+'&search='+tag  #constructing the search URL, adding the user's desired criteria
             elif (tag == "NULL" and inc_ingr != "NULL" and exc_ingr != "NULL"):
                 url = 'https://www.allrecipes.com/search/results/?IngExcl=' + exc_ingr + '&IngIncl=' + inc_ingr  # constructing the search URL, adding the user's desired criteria
+            elif(tag == "NULL" and inc_ingr == "NULL" and exc_ingr == "NULL"):  #if nothing is selected, user is redirected to the selection page until something is chosen
+                return render_template('home.html')
             else:
                 url = 'https://www.allrecipes.com/search/results/?search='+tag  #constructing the search URL, adding the user's desired criteria
+
             reqs = requests.get(url)
-            soup = BeautifulSoup(reqs.text, 'lxml')
+            soup = BeautifulSoup(reqs.text, 'lxml') #using Beautiful Soup
 
             links = [link['href'] for link in soup.select('a[href*="https://www.allrecipes.com/recipe/"]')] #putting the links to recipes into a list
-            unique_links=set(links) #storing the list in a set so there are only unique links
+            unique_links=set(links) #storing the list in a set so there are only unique links (if I do not do this, there are two of the same link)
 
             with sql.connect("recipeData.db") as con:
                 cur = con.cursor()
                 i=0
                 for link in unique_links:
-                    if(i==10):  #cutting off results at 10 to try to cut down loading time
+                    if(i==10):  #cutting off results at 10
                         break
 
                     scraper = scrape_me(link)
@@ -119,7 +134,7 @@ def get_recipes():
                     else:
                         time=str(hours)+" hrs "+str(min)+" mins"
 
-                    image=scraper.image()
+                    image=scraper.image()   #recipe image
 
                     cur.execute("INSERT INTO recipes (Image, RecipeName, Time, Yields, Ingredients, Instructions, URL) VALUES (?,?,?,"  #inserting data into table
                             "?,?,?,?)", (image,scraper.title(), time, scraper.yields(), ingredients, scraper.instructions(), link))
@@ -129,14 +144,14 @@ def get_recipes():
             con.rollback()
             print("Error in insert operation")
 
-        finally:
-            conn = sql.connect("recipeData.db")
-            conn.row_factory = sql.Row
-            cur = conn.cursor()
-            cur.execute(f"select * from recipes")
-            revRows = cur.fetchall();
-            return render_template("recipes.html", revRows=revRows)
-    if request.method == 'GET':
+        conn = sql.connect("recipeData.db") #displaying table
+        conn.row_factory = sql.Row
+        cur = conn.cursor()
+        cur.execute(f"select * from recipes")
+        revRows = cur.fetchall();
+        return render_template("recipes.html", revRows=revRows)
+
+    if request.method == 'GET': #only displays the table
         conn = sql.connect("recipeData.db")
         conn.row_factory = sql.Row
         cur = conn.cursor()
